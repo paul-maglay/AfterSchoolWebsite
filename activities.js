2a185f4bb2eae4894cd92efe9fcdcf7ae5d5b12d
paul-maglay <paul.maglay@gmail.com>
2024-12-07 18:50:32 +0400

    Add files via upload

---
+++ activities.js
@@ -0,0 +1,88 @@
+let afterSchool = [
+    {
+        "id": 1001,
+        "subject":'Board Games',
+        "location":'Germany',
+        "price":300,
+        "spaces":5,
+        "image":'images/board.png',
+    },
+    {
+        "id": 1002,
+        "subject":'Book Club',
+        "location":'Philippines',
+        "price":200,
+        "spaces":5,
+        "image":'images/book.png',
+    },
+    {
+        "id": 1003,
+        "subject":'Culinary',
+        "location":'India',
+        "price":500,
+        "spaces":5,
+        "image":'images/cooking.png',
+    },
+    {
+        "id": 1004,
+        "subject":'Football Club',
+        "location":'Indonesia',
+        "price":600,
+        "spaces":5,
+        "image":'images/football.png',
+    },
+    {
+        "id": 1005,
+        "subject":'Gardening',
+        "location":'USA',
+        "price":700,
+        "spaces":5,
+        "image":'images/gardening.png',
+    },
+    {
+        "id": 1006,
+        "subject":'Arts & Crafts',
+        "location":'UK',
+        "price":100,
+        "spaces":5,
+        "image":'images/painting.png',
+    },
+    {
+        "id": 1007,
+        "subject":'Photography Club',
+        "location":'Australia',
+        "price":150,
+        "spaces":5,
+        "image":'images/photography.png',
+},
+    {
+        "id": 1008,
+        "subject":'Robotics Club',
+        "location":'Spain',
+        "price":1000,
+        "spaces":5,
+        "image":'images/robotics.png',
+},
+    {
+        "id": 1009,
+        "subject":'Swimming Club',
+        "location":'France',
+        "price":300,
+        "spaces":5,
+        "image":'images/swimming.png',
+},
+    {
+        "id": 1010,
+        "subject":'Theater Club',
+        "location":'Italy',
+        "price":350,
+        "spaces":5,
+        "image":'images/theater.png',
+},
+
+
+
+
+
+
+]
